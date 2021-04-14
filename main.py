de3ef4cec313093a10543263c9c5f4cd1d9b17aa
Albert Ma <albert.ma@ymail.com>
2021-04-15 00:24:22 +0100

    feat: add basket

--- main.py
+++ main.py
@@ -19,14 +19,18 @@ class Game:
         self.end_t = pg.time.get_ticks()
         self.fruits_list = ["apple", "banana", "cherry", "grapes", "green_apple", "lemon",
                             "mango", "orange", "pear", "strawberry", "watermelon"]
+        self.basket = Basket(self.scn_width, self.scn_height)
 
     def main(self):
         while self.running:
             for event in pg.event.get():
                 if event.type == QUIT:
                     self.running = False
+            keys = pg.key.get_pressed()
+            self.basket.move(keys)
             self.screen.blit(self.bgpic, (0, 0))
             self.end_t = pg.time.get_ticks()
+            self.screen.blit(self.basket.img, self.basket.img_rect)
             if self.end_t - self.start_t > 2000:
                 self.start_t = self.end_t
                 fname = random.choice(self.fruits_list)
@@ -55,6 +59,23 @@ class Fruit:
 
     def drop(self):
         self.img_rect.move_ip(self.x_speed, self.y_speed)
+
+
+class Basket:
+    def __init__(self, scn_width, scn_height):
+        self.x_speed = 9
+        self.img = pg.image.load("img/basket.png")
+        self.img = pg.transform.scale(self.img, (int(self.img.get_width()/2), int(self.img.get_height()/2)))
+        self.img_rect = self.img.get_rect()
+        self.img_rect.bottom = scn_height
+        self.img_rect.centerx = scn_width / 2
+
+    def move(self, keys):
+        if not (keys[K_LEFT] and keys[K_RIGHT]):
+            if keys[K_LEFT]:
+                self.img_rect.move_ip(-self.x_speed, 0)
+            elif keys[K_RIGHT]:
+                self.img_rect.move_ip(self.x_speed, 0)
 
 
 if __name__ == "__main__":
