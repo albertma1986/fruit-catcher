80932696e73bbb05e5987e0804509b153c61fbbb
Albert Ma <albert.ma@ymail.com>
2021-04-14 23:32:16 +0100

    fix: correct image path name

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame as pg
 from pygame.locals import *
 import random
 
-
 class Game:
     def __init__(self):
         pg.init()
@@ -11,7 +10,7 @@ class Game:
         self.scn_height = 720
         self.FPS = 60
         self.clock = pg.time.Clock()
-        self.bgpic = pg.image.load("bg01.jpg")
+        self.bgpic = pg.image.load("img/bg01.jpg")
         self.bgpic = pg.transform.scale(self.bgpic, (self.scn_width, self.scn_height))
         self.screen = pg.display.set_mode((self.scn_width, self.scn_height))
         self.fruits = []
@@ -47,7 +46,7 @@ class Fruit:
         self.kind = kind
         self.x_speed = 0
         self.y_speed = 3
-        self.img = pg.image.load(kind+".png")
+        self.img = pg.image.load("img/" + kind + ".png")
         self.img = pg.transform.scale(self.img, (int(self.img.get_width()/2), int(self.img.get_height()/2)))
         self.img_rect = self.img.get_rect()
 
